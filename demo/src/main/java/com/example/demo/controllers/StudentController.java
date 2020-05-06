bf97c9381efb8d446ee639e2beab5cdc23968a53
TL <TL@gmail.com>
2020-05-06 17:52:47 +0530

    m4 added

--- demo/src/main/java/com/example/demo/controllers/StudentController.java
+++ demo/src/main/java/com/example/demo/controllers/StudentController.java
@@ -6,16 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentController {
 
-	
 	@RequestMapping("/welcome")
 	public String testMethoad(Integer I) {
 
 		return "welcome controller";
 	}
+
 	public void m1() {
-		System.out.println("i'm happy"); 
-		}
+		System.out.println("i'm happy");
+	}
+
 	public void m4() {
 		System.out.println("m4 created ");
 	}
+	
 }
